fde9ad37206238ee4e3512cc4afc8f54d71b43aa
AYB <ayyubiy67@gmail.com>
2024-12-02 11:08:48 +0100

    Add Layout component for structuring the main layout of the social media scheduler project

---
+++ src/components/Layout.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { Sidebar } from './Sidebar';
+import { TopNav } from './TopNav';
+import { useLocation } from 'wouter';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [, setLocation] = useLocation();
+
+  useEffect(() => {
+    setIsSidebarOpen(false);
+  }, [setLocation]);
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.body.classList.add('mobile-menu-open');
+    } else {
+      document.body.classList.remove('mobile-menu-open');
+    }
+    return () => {
+      document.body.classList.remove('mobile-menu-open');
+    };
+  }, [isSidebarOpen]);
+
+  return (
+    <div className="min-h-screen bg-background">
+      <TopNav onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)} />
+      
+      <div className="flex pt-16">
+        {isSidebarOpen && (
+          <div
+            className="fixed inset-0 bg-gray-600 bg-opacity-75 z-20 lg:hidden"
+            onClick={() => setIsSidebarOpen(false)}
+          />
+        )}
+
+        <div
+          className={`fixed inset-y-0 left-0 transform ${
+            isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+          } lg:relative lg:translate-x-0 transition duration-200 ease-in-out z-30 lg:z-0 top-0`}
+        >
+          <Sidebar onClose={() => setIsSidebarOpen(false)} />
+        </div>
+
+        <main className="flex-1 px-2 py-6 lg:pl-5 lg:ml-64 overflow-x-hidden">
+          <div className="max-w-[1400px] mx-auto">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
