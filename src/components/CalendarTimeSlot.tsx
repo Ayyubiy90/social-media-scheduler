d36d218ce0591b6fed69c2a5437d4b491a87d7b5
AYB <ayyubiy67@gmail.com>
2024-12-02 11:10:43 +0100

    Add CalendarTimeSlot component for droppable areas in the calendar view of the social media scheduler project

---
+++ src/components/CalendarTimeSlot.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDroppable } from '@dnd-kit/core';
+
+interface CalendarTimeSlotProps {
+  id: string;
+  date: Date;
+  children: React.ReactNode;
+}
+
+export function CalendarTimeSlot({ id, children }: CalendarTimeSlotProps) {
+  const { setNodeRef } = useDroppable({
+    id,
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      className="h-20 border-b border-gray-200 dark:border-gray-700 p-1 bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-700"
+    >
+      {children}
+    </div>
+  );
+}
